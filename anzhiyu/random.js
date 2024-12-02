129c10bbe60369edda59f4485474726dc88dc8c6
SmallHao-pro <s1mallhao@163.com>
2024-12-02 13:27:32 +0800

    Site updated: 2024-12-02 13:27:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/02/hello-world/"];function toRandomPost(){
+var posts=["2024/12/02/Hao新开通博客/","2024/12/02/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
