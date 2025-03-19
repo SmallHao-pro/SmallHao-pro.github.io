8f289bce24da7e03eb9284e26f71095a77984e0b
SmallHao-pro <s1mallhao@163.com>
2025-03-19 22:22:14 +0800

    Site updated: 2025-03-19 22:22:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["content/平台搭建-DNSLog/","content/内网渗透-免杀/","content/信息收集-敏感信息收集/"];function toRandomPost(){
+var posts=["content/MySQL注入技巧/","content/平台搭建-DNSLog/","content/内网渗透-免杀/","content/信息收集-敏感信息收集/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
